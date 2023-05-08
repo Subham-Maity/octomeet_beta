eeb35fbdc0fc3398f83db03d7140e0e318e73f9e
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-05-08 18:33:21 +0530

    AuthForm → Login and Register , isLoading, setLoading, toggleVariant

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useCallback, useState } from "react";
+
+type Variant = "LOGIN" | "REGISTER";
+
+export const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  return <div>Auth Form!</div>;
+};
